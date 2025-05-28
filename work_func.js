e643c4403c48560537a9ac06ca70e7bb4899b23a
jiminseon <jiminseon@jiminseonui-MacBookPro.local>
2025-05-28 15:41:27 +0900

    work_func

---
+++ work_func.js
@@ -0,0 +1,13 @@
+function ask(questionFn, yesFn, noFn) {
+    if (questionFn()) {
+        yesFn();
+    } else {
+        noFn();
+    }
+}
+
+ask(
+        () => confirm("동의하십니까?"),
+        function() { alert("동의하셨습니다.") },
+        function() { alert("취소 버튼을 누르셨습니다.") }
+    );
