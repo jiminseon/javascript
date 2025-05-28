9eefc3a3952737c52af934b1ec3d73ed492fe80a
jiminseon <pitapatsun@gmail.com>
2025-05-28 16:09:30 +0900

    work2_func #3

---
+++ work2-func.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    return a + b;
+}
+
+function withLoggin(fn) {
+    return function(...args) {
+        console.log("[" + args + "]를 인자로 전달한 함수 호출")
+        return fn(...args)
+    }
+}
+
+const addWithLogging = withLogging(add);
+const result = addWithLogging(1, 2); // 로그 출력 후 3 반환
+console.log("결과:", result); // → 결과: 3
