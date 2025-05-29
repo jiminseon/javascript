ee23c209e69c5fc7bea8268c368b86e0faf37015
jiminseon <pitapatsun@gmail.com>
2025-05-29 09:24:31 +0900

    #8 구조분해및할당

---
+++ obj4.js
@@ -0,0 +1,15 @@
+const width = 100;
+const height = 200;
+
+const obj1 = {
+    width : width,
+    height : height
+};
+
+const obj2 = {
+    width,
+    height
+};
+
+console.log(obj1);
+console.log(obj2);
