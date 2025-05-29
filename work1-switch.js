f51579318b8363ee44cf3634fe84e2801d903d55
jiminseon <jiminseon@jiminseonui-MacBookPro.local>
2025-05-29 10:22:35 +0900

    #8 condition

--- work1-switch.js
+++ work1-switch.js
@@ -1,12 +1,10 @@
-if (browser === 'Edge') {
-    console.log("Edge를 사용하고 계시네요!");
-} else if (
-    browser === 'Chrome' ||
-    browser === 'Firefox' ||
-    browser === 'Safari' ||
-    browser === 'Opera'
-) {
-    console.log('저희 서비스가 지원하는 브라우저를 사용하고 계시네요.');
-} else {
-    alert('현재 페이지가 괜찮아 보이길 바랍니다!');
+
+function detectBrowser(browser){
+    if (browser === 'Edge'){
+        console.log("Edge를 사용중이시네요.");
+    } else if (["Chrome", "Firefox", "Safari", "Opera"].includes(browser)){
+        console.log("저희 서비스가 지원하는 브라우저를 사용중이시네요.")
+    } else{
+        alert("현재 페이지가 괜찮아 보이길 바랍니다!")
+    }
 }
