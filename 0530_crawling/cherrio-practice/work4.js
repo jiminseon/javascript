1c924c56a206b21ac9b7484d1f93340550a984bd
jiminseon <pitapatsun@gmail.com>
2025-06-02 13:53:34 +0900

    #13 연습문제(4) page 2까지

--- 0530_crawling/cherrio-practice/work4.js
+++ 0530_crawling/cherrio-practice/work4.js
@@ -5,43 +5,43 @@ const url = "https://finance.naver.com/item/sise.nhn?code=005930";
 const baseurl = "https://finance.naver.com";
 
 async function siseUrl(siseurl) {
-  const sise = await fetch(baseurl + siseurl, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    },
-  });
-  const body = await sise.text();
-  const $day = cheerio.load(body);
-
-  const lists = $day("tr").has("td.num");
-  console.log(lists.length);
-  console.log(lists.eq(0).find(".tah").eq(0).text());
-  console.log(lists.eq(0).find(".tah").length);
-
   const siseData = [];
-
-  for (let i = 0; i < lists.length; i++) {
-    const listNode = lists.eq(i).find(".tah");
-    const date = listNode.eq(0).text(); // 신문사
-    const finalPrice = listNode.eq(1).text(); // 제목
-    const change = listNode.eq(2).text().trim(); //요약설명
-    const price = listNode.eq(3).text(); //날짜
-    const high = listNode.eq(4).text(); // url
-    const low = listNode.eq(5).text(); // url
-    const trading = listNode.eq(6).text(); // url
-
-    siseData.push({
-      date,
-      finalPrice,
-      change,
-      price,
-      high,
-      low,
-      trading,
+  for (let page = 1; page <= 2; page++) {
+    const sise = await fetch(baseurl + siseurl + `&page=${page}`, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+      },
     });
-  }
+    const body = await sise.text();
+    const $day = cheerio.load(body);
 
+    const lists = $day("tr").has("td.num");
+    console.log(lists.length);
+    console.log(lists.eq(0).find(".tah").text());
+    console.log(lists.eq(0).find(".tah").length);
+
+    for (let i = 0; i < lists.length; i++) {
+      const listNode = lists.eq(i).find(".tah");
+      const date = listNode.eq(0).text(); // 신문사
+      const finalPrice = listNode.eq(1).text(); // 제목
+      const change = listNode.eq(2).text().trim(); //요약설명
+      const price = listNode.eq(3).text(); //날짜
+      const high = listNode.eq(4).text(); // url
+      const low = listNode.eq(5).text(); // url
+      const trading = listNode.eq(6).text(); // url
+
+      siseData.push({
+        date,
+        finalPrice,
+        change,
+        price,
+        high,
+        low,
+        trading,
+      });
+    }
+  }
   // JSON으로 변환
   const data = JSON.stringify(siseData, null, 2);
 
