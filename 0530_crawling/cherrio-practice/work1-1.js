e3f8421bca7f4b70ce2179953c7ecc4f72560029
jiminseon <pitapatsun@gmail.com>
2025-06-02 10:41:03 +0900

    #13 연습문제(1) 강사님 코드 추가

--- 0530_crawling/cherrio-practice/work1-1.js
+++ 0530_crawling/cherrio-practice/work1-1.js
@@ -59,3 +59,51 @@ const url = "https://quotes.toscrape.com/";
     console.error(err);
   }
 })();
+
+//---------------------------------------------
+//---------------------------------------------
+//---------------------------------------------
+//강사님
+import * as cheeio from "cheerio";
+import * as fs from "fs";
+
+const baseUrl = "https://quotes.toscrape.com/";
+
+async function main() {
+  const result = [];
+
+  // 1. request -> response
+  const response = await fetch(baseUrl);
+  const html = await response.text();
+  // 2. response를 parsing
+  const $ = cheeio.load(html);
+  // 3. (quote:string, authorName:string, tags:string[])
+  const quoteTags = $(".quote");
+
+  for (let i = 0; i < quoteTags.length; i++) {
+    const quoteTag = quoteTags.eq(i);
+
+    const quote = quoteTag.find(".text").text().trim();
+    const authorName = quoteTag.find(".author").text().trim();
+
+    const tags = [];
+
+    const tagTags = quoteTag.find(".tag");
+    for (let j = 0; j < tagTags.length; j++) {
+      const tagText = tagTags.eq(j).text().trim();
+      tags.push(tagText);
+    }
+
+    result.push({
+      quote,
+      authorName,
+      tags,
+    });
+  }
+  // 4. json으로 저장
+  // JS => JSON문자열로 변환
+  const jsonResult = JSON.stringify(result);
+  await fs.promises.writeFile("quote1.json", jsonResult);
+}
+
+main();
--- 0530_crawling/cherrio-practice/work1-3.js
+++ 0530_crawling/cherrio-practice/work1-3.js
@@ -58,3 +58,85 @@ async function fetchPage(url) {
     console.error(err);
   }
 })();
+
+//---------------------------------------------
+//---------------------------------------------
+//---------------------------------------------
+// 강사님
+import * as cheeio from "cheerio";
+import * as fs from "fs";
+
+const baseUrl2 = "https://quotes.toscrape.com/";
+
+const authorDescCache = {
+  // key: url
+  // value: authorDesc
+};
+
+async function fetchAuthorDesc(authorHref) {
+  if (authorDescCache[authorHref]) {
+    return authorDescCache[authorHref];
+  }
+  const authorUrl = baseUrl + authorHref;
+  const authorResp = await fetch(authorUrl);
+  const authroHtml = await authorResp.text();
+  const $author = cheeio.load(authroHtml);
+  const authorDescription = $author(".author-description").text().trim();
+
+  authorDescCache[authorHref] = authorDescription;
+
+  return authorDescription;
+}
+
+async function main() {
+  const result = [];
+  let url = baseUrl2;
+
+  while (true) {
+    // 1. request -> response
+    const response = await fetch(url);
+    const html = await response.text();
+    // 2. response를 parsing
+    const $ = cheeio.load(html);
+    // 3. (quote:string, authorName:string, tags:string[])
+    const quoteTags = $(".quote");
+
+    for (let i = 0; i < quoteTags.length; i++) {
+      const quoteTag = quoteTags.eq(i);
+
+      const quote = quoteTag.find(".text").text().trim();
+      const authorName = quoteTag.find(".author").text().trim();
+      const authorHref = quoteTag.find("span a").attr("href");
+      const authorDescription = await fetchAuthorDesc(authorHref);
+
+      const tags = [];
+
+      const tagTags = quoteTag.find(".tag");
+      for (let j = 0; j < tagTags.length; j++) {
+        const tagText = tagTags.eq(j).text().trim();
+        tags.push(tagText);
+      }
+
+      result.push({
+        quote,
+        authorName,
+        tags,
+        authorDescription,
+      });
+    }
+
+    const nextATag = $(".next a");
+    const href = nextATag.attr("href");
+    if (!href) {
+      break;
+    }
+    url = baseUrl + href;
+  }
+
+  // 4. json으로 저장
+  // JS => JSON문자열로 변환
+  const jsonResult = JSON.stringify(result);
+  await fs.promises.writeFile("quote3.json", jsonResult);
+}
+
+main();
