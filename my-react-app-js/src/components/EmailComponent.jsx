5300676eda9edef82dfe214ea742ea0f6ec12df9
jiminseon <pitapatsun@gmail.com>
2025-06-05 13:42:58 +0900

    #17 연습문제6

---
+++ my-react-app-js/src/components/EmailComponent.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export default function EmailComponent() {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+
+      const info = data.map((user) => ({
+        username: user.username,
+        email: user.email,
+      }));
+
+      setInfo(info);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <ul style={{ marginTop: "20px" }}>
+        {info.map((user, idx) => (
+          <li key={idx}>
+            {user.username}: {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
