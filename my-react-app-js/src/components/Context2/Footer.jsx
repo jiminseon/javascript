254ce3098e794eaace62fd513c989287ca544b1a
jiminseon <pitapatsun@gmail.com>
2025-06-10 16:45:16 +0900

    #22 Context 강사님 설명2

--- my-react-app-js/src/components/Context2/Footer.jsx
+++ my-react-app-js/src/components/Context2/Footer.jsx
@@ -7,7 +7,18 @@ export default function Footer() {
   return (
     <div>
       Footer
-      <div>유저이름:{ctx.user ? ctx.user.name : "X"}</div>
+      <div>유저이름: {ctx.user ? ctx.user.name : "X"}</div>
     </div>
   );
 }
+
+// export vs export default의 차이
+// import할 때 어떻게 가지고 오느냐?
+
+// export일 경우 import시 중괄호 필요.(이름 같게 해서)
+// export default 일 경우 import시 중괄호 필요없음. (이름 달라도 된다.)
+
+// export const myValue = 10; // import {myValue} from 'File'
+// export default myValue2 = 10; // import MyCustomValue from 'File'
+
+export const myValue = 10;
--- my-react-app-js/src/components/Context2/Navbar.jsx
+++ my-react-app-js/src/components/Context2/Navbar.jsx
@@ -1,15 +1,19 @@
-import React, { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "./ContextApp";
 
 export default function Navbar() {
   const ctx = useContext(UserContext);
   console.log("Navbar", ctx);
+
+  const [inputText, setInputText] = useState("");
+
   return (
     <div>
       Navbar
       <div>
-        <input type="text" />
-        <button>로그인</button>
+        <input type="text" onChange={(e) => setInputText(e.target.value)} />
+
+        <button onClick={() => ctx.login(inputText)}>로그인</button>
       </div>
     </div>
   );
