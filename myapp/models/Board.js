8827ae40929842871a719e407b8e0b38184eb608
jiminseon <pitapatsun@gmail.com>
2025-06-13 15:35:36 +0900

    feat: get,post #28

--- myapp/models/Board.js
+++ myapp/models/Board.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const boardSchema = new mongoose.Schema({
   title: {
     type: String,
--- myapp/routes/board.js
+++ myapp/routes/board.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const Board = require("./modls/Board");
-
+const Board = require("../models/Board");
 /**
  *
  */
-router.get("/", function (req, res, next) {
-  console.log("router ./board 실행");
-  res.send("this is Board");
+router.get("/", async function (req, res, next) {
+  //   console.log("router ./board 실행");
+  //   res.send("this is Board");
+  const board = await Board.find();
+  res.send(board);
 });
 
-route.post("/", function (req, res) {
-  Board.create({
-    title: "임시 게시글",
-    content: "임시 게시글 내용",
+router.post("/", async function (req, res) {
+  /**
+   * 1. req body로 "게시글 제목"과 "게시글 내용"을 받는다.
+   * 2. mongoose를 이용해 저장한다.
+   * 3. response를 만들어 준다.
+   *
+   */
+  const data = req.body;
+  console.log(data);
+  const board = await Board.create({
+    title: data.title,
+    content: data.content,
   });
+
+  res.json(board);
 });
 
 module.exports = router;
